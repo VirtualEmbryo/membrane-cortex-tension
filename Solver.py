809be649dec9d4b87c7fddf5045aa3f12daf7810
Hudson Borja da Rocha <hudson.borja-da-rocha@college-de-france.fr>
2022-08-30 12:13:54 +0100

    First commit to GitLab

--- Solver.py
+++ Solver.py
@@ -1,4 +1,26 @@
 from dolfin import *
+# 
+#     Solve the 1D system of equations for the membrane-cortex mechanics
+
+#             dX/dt + (eta/mu) d^2 V/dxi^2 + (1/mu)dSigma/dx - V  = 0
+#             dX/dt - (k/mu )d^2 X/dxi^2 -v = 0
+#             x(\xi = 0) = 0, and x(\xi = L) = \deltaL
+#             v(\xi = 0) = 0, and v(\xi = L) = \bar v
+
+#         \xi: the spatial coordinate
+#         t : time coordinate
+#         x  : Displacement field of the plasma membrane [um]
+#         v  : velocity field of the actin cortex [um/s]
+#         eta : 2D effective viscosity of the cortex [pN s / um]
+#         k : Elastic constant of the membrane [pN/um]
+#         mu : Friction coefficient of the cortex-membrane interaction [pN s/ um^3]
+
+#     The trap force is simulated by a Dirichlet boundary condition on the displacement field x(\xi = L)
+#     The active force that comes from the optogenetic manipulations can be simulated either by a Dirichlet bc on v(\xi =L) = \bar v
+        
+#     We solve the system for the displacement x(\xi, t) and velocity v(\xi, t) fields.
+
+# 
 class Solver:
     def __init__(
         self,
@@ -48,13 +70,17 @@ class Solver:
             return near(x[0], self.L) and on_boundary
 
 
+        # self.bc = ([DirichletBC(self.Q, Constant((0.,0.)), left_end),
+        #             DirichletBC(self.Q.sub(1), self.imposed_velocity, right_end),
+        #             DirichletBC(self.Q.sub(0), self.imposed_displacement, right_end)])
+
+
         self.bc = ([DirichletBC(self.Q, Constant((0.,0.)), left_end),
-                    DirichletBC(self.Q.sub(1), self.imposed_velocity, right_end),
-                    DirichletBC(self.Q.sub(0), self.imposed_displacement, right_end)])
+                    DirichletBC(self.Q.sub(1), self.imposed_velocity, right_end)])
     
-    def solve(self):
+    def solve(self, bc):
         # self.set_geometry_and_function_space()
-        self.set_boundary_conditions()
+        # self.set_boundary_conditions()
         weak_form = (
                     ((self.x_ - self.x_old) / self.time_step_length) * self.dv_ * dx + \
                     - (self.eta / self.mu) * dot(grad(self.v_), grad(self.dv_)) * dx + \
@@ -66,7 +92,7 @@ class Solver:
         weak_form_lhs = lhs(weak_form)
         weak_form_rhs = rhs(weak_form)
         
-        solve(weak_form_lhs == weak_form_rhs, self.q_solution, self.bc)
+        solve(weak_form_lhs == weak_form_rhs, self.q_solution, bc)
         self.x_s, self.v_s = self.q_solution.split(deepcopy=True)
         self.x_old = project(self.q_solution.sub(0),self.Q.sub(0).collapse())
 
